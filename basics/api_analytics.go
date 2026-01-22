e3948f86e5527e6813f39a77231913ed9e0690ca
Nikko Dutra Bouck <nikkodutra@icloud.com>
2026-01-22 12:30:35 -0800

    Memory allocation experiment

--- basics/api_analytics.go
+++ basics/api_analytics.go
@@ -2,9 +2,9 @@ package basics
 
 import (
 	"encoding/json"
-	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func requestHistory() []string {
@@ -26,17 +26,23 @@ type ApiInfo struct {
 	AvgLatency   int
 }
 
-func runAPI() {
+func runHistoryAnaliticsA() (string, int64) {
 
+	start := time.Now()
 	logHistory := requestHistory()
+
+	// We know the number of elements we need so lets initialize the memory
 	logs := make([][]string, len(logHistory))
 
 	for i, log := range logHistory {
 		logs[i] = strings.Fields(log)
 	}
 
-	apiMap := make(map[string]ApiInfo, len(logs))
-	apiLatency := map[string]int{}
+	// This might have only 1 entry
+	apiMap := map[string]ApiInfo{}
+
+	// We know the number of elements we need so lets initialize the memory
+	apiLatency := make(map[string]int, len(logs))
 
 	for _, logArr := range logs {
 
@@ -65,11 +71,67 @@ func runAPI() {
 
 	}
 
+	t := time.Now()
+	elapsed := t.Sub(start)
+
+	//fmt.Println("\n------------- runHistoryAnaliticsFast() -------------")
+	//fmt.Println("Good memory management results -> ", elapsed)
+
 	outMap := &apiMap
-	derefrenceApiMap, _ := json.Marshal(outMap)
+	jsonApiMap, _ := json.Marshal(outMap)
 
-	fmt.Println("JSON: ", string(derefrenceApiMap))
+	return string(jsonApiMap), elapsed.Nanoseconds()
 
-	fmt.Println(apiMap)
+}
+
+func runHistoryAnaliticsB() (string, int64) {
+	start := time.Now()
+
+	logHistory := requestHistory()
+
+	// This might have only 1 entry
+	apiMap := map[string]ApiInfo{}
+
+	// We know the number of elements we need so lets initialize the memory
+	apiLatency := make(map[string]int, len(logHistory))
+
+	for _, log := range logHistory {
+
+		logArr := strings.Fields(log)
+
+		key := logArr[0]
+		latency, _ := strconv.Atoi(logArr[2])
+		apiLatency[key] += latency
+
+		_, keyExists := apiMap[key]
+		if keyExists {
+
+			entry := apiMap[key]
+			entry.SuccessCount++
+
+			avgLatency := apiLatency[key] / entry.SuccessCount
+			entry.AvgLatency = avgLatency
+
+			apiMap[key] = entry
+
+			continue
+		}
+
+		apiMap[key] = ApiInfo{
+			SuccessCount: 1,
+			AvgLatency:   latency,
+		}
+
+	}
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+	//fmt.Println("\n------------- runHistoryAnaliticsSlow() -------------")
+	//fmt.Println("Bad memory management results -> ", elapsed)
+
+	outMap := &apiMap
+	jsonApiMap, _ := json.Marshal(outMap)
+
+	return string(jsonApiMap), elapsed.Nanoseconds()
 
 }
--- basics/api_analytics_test.go
+++ basics/api_analytics_test.go
@@ -1,7 +1,76 @@
 package basics
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAPIAnalytics(t *testing.T) {
-	runAPI()
+
+	fmt.Println("------------ RUN 1 ------------")
+	responseA, elapsedA := runHistoryAnaliticsA()
+	responseB, elapsedB := runHistoryAnaliticsB()
+
+	fmt.Println("A time: ", elapsedA, "response:", responseA)
+	fmt.Println("B time: ", elapsedB, "response:", responseB)
+	if responseA != responseB {
+		t.Errorf("Expected same output, got different outputs")
+	}
+
+	fmt.Println("------------ RUN 2 ------------")
+	responseB, elapsedB = runHistoryAnaliticsB()
+	responseA, elapsedA = runHistoryAnaliticsA()
+	fmt.Println("A time: ", elapsedA, "response:", responseA)
+	fmt.Println("B time: ", elapsedB, "response:", responseB)
+
+	fmt.Println("------------ RUN 3 ------------")
+	responseB, elapsedB = runHistoryAnaliticsB()
+	responseA, elapsedA = runHistoryAnaliticsA()
+	fmt.Println("A time: ", elapsedA, "response:", responseA)
+	fmt.Println("B time: ", elapsedB, "response:", responseB)
+
+	fmt.Println("------------ RUN 4 ------------")
+	responseA, elapsedA = runHistoryAnaliticsA()
+	responseB, elapsedB = runHistoryAnaliticsB()
+	fmt.Println("A time: ", elapsedA, "response:", responseA)
+	fmt.Println("B time: ", elapsedB, "response:", responseB)
+
+	fmt.Println("\n------------ RUN Averages ------------")
+	runs := int64(100000)
+	fmt.Println("Run each function: ", runs, " times and take the average.")
+
+	aAvg := runTestBlock(runHistoryAnaliticsA, runs)
+	bAvg := runTestBlock(runHistoryAnaliticsB, runs)
+
+	bAvg += runTestBlock(runHistoryAnaliticsB, runs)
+	aAvg += runTestBlock(runHistoryAnaliticsA, runs)
+
+	bAvg += runTestBlock(runHistoryAnaliticsB, runs)
+	aAvg += runTestBlock(runHistoryAnaliticsA, runs)
+
+	aAvg = aAvg / 3
+	bAvg = bAvg / 3
+
+	fmt.Println("A Avg: ", aAvg, "ns --- B Avg: ", bAvg, "ns")
+}
+
+func runTestBlock(f func() (string, int64), runs int64) int64 {
+	ch := make(chan int64)
+
+	go func(f func() (string, int64), runs int64) {
+		total := int64(0)
+		for range runs {
+			total += timeTest(f)
+		}
+		ch <- total / runs
+	}(f, runs)
+
+	return <-ch
+}
+
+func timeTest(f func() (string, int64)) int64 {
+
+	_, time := f()
+
+	return time
 }
